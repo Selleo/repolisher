46f9e8d89e8ec7a670ebedccd0e595e28de24948
Kamil Korczynski <korczas+github@gmail.com>
2020-11-29 13:51:51 +0100

    test: rome-defaults command

--- test/commands/rome-defaults.test.ts
+++ test/commands/rome-defaults.test.ts
@@ -1,17 +1,20 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
+import * as inquirer from 'inquirer'
+import * as glob from 'glob'
+import * as fs from 'fs'
+import * as child_process from 'child_process'
+const sinon = require('sinon')
 
 describe('rome-defaults', () => {
-  test
-  .stdout()
-  .command(['rome-defaults'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+  sinon.stub(inquirer, 'prompt').returns(Promise.resolve({ 'rome-defaults': 'Yes' }))
+  sinon.stub(glob, 'sync').callsFake(() => [])
+  sinon.stub(fs, 'readFileSync').callsFake(() => '')
+  sinon.stub(child_process, 'exec').yields(undefined)
 
   test
-  .stdout()
-  .command(['rome-defaults', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['rome-defaults'])
+    .it('Prints table with statuses and list options', ctx => {
+      expect(ctx.stdout).to.contain('Files generated successfully')
+    })
 })
